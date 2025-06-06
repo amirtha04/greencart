12205c75487cf85dbe388f4221488ecc88ad077d
amirtha04 <AMIR22008.IT@rmkec.ac.in>
2025-06-06 16:43:14 +0530

    first commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,22 @@
+import { createContext ,useContext} from "react";
+import {useNavigate} from "react-router-dom";
+import { useState } from "react";
+
+export const AppContext=createContext();
+
+export const AppContextProvider=({children})=>{
+   
+    const navigate=useNavigate();
+   const [user,setUser]=useState(null);
+   const [isSeller,setIsSeller]=useState(false);
+
+
+    const value={navigate,user,setUser,setIsSeller,isSeller}
+   return <AppContext.Provider value={value}>
+    {children}
+   </AppContext.Provider>
+}
+
+export const useAppContext=()=>{
+    return useContext(AppContext)
+}
