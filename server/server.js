d62780339c814de6bee8c387b060a26159e7f966
amirtha04 <AMIR22008.IT@rmkec.ac.in>
2025-06-17 00:38:09 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -3,11 +3,17 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/productRoute.js';
+
 
 const app=express();
 const port=process.env.PORT || 4000;
 
 await connectDB()
+await connectCloudinary()
 
 
 //Allow multiple origins
@@ -21,6 +27,10 @@ app.use(cors({origin: allowedOrigins,Credentials: true}));
 
 
 app.get('/', (req,res) =>res.send("API is working"));
+app.use('/api/user',userRouter)
+app.use('/api/seller',sellerRouter)
+app.use('/api/product',productRouter)
+
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
